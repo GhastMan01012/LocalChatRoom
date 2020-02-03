cb07cb16e9b20e0b05f8de2ee8e70627a33ba2b7
Ethan Foley-Lewis <efoleylewis@gmail.com>
2020-02-03 14:18:04 +1000

    Update encrypt.php

--- encrypt.php
+++ encrypt.php
@@ -1,17 +1,7 @@
 <?php session_start();// Start a session where $_SESSION[] variables can be called from.
 
 include_once 'functions/customUsernames.php';
-
-// Define a function to add all the default values to the UserSettings.Colours table
-function defaultify($userName) {
-  $link = mysqli_connect("127.0.0.1", "root", "root", "UserSettings");
-  $sql = "INSERT INTO Colours (UserName, MainBubble, MainBubbleFont, SecondaryBubble, SecondaryBubbleFont, BackgroundColour, AccentColour, HeaderColour, GeneralColour) VALUES ('".$userName."', '#d51c46', '#ffffff', '#dfdfdf', '#116280', '#dfdfdf', '#d51c46', '#116280', '#0696cc')";
-    if(!mysqli_query($link, $sql)) {
-        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-    }
-  // Close connection
-  mysqli_close($link);
-}
+include_once 'functions/registration.php';
 
 // Encrypt the set password and write it to the file. (When the client registers and account)
 if(isset($_POST['registerPassword'])) {
