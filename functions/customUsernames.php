444649ed8dd2e07f008e4f44f952b618fffe9fb5
Ethan Foley-Lewis <efoleylewis@gmail.com>
2020-02-03 12:30:59 +1000

    A sheet of functions for usernames
    This can only take a string and colour it like a rainbow. More functionality will be added later.

--- functions/customUsernames.php
+++ functions/customUsernames.php
@@ -1,5 +1,29 @@
 <?php
 function rainbow($string) {
-    
+    $x = str_split($string);
+    $j = 1;
+    $newString  = "";
+    for($i = 0; $i < count($x); $i++) {
+        if($j == 1) {
+            $newString = $newString . "<a style='color:rgb(255,0,0)'>" . $x[$i] . "</a>";
+            $j++;
+        } elseif($j == 2) {
+            $newString = $newString . "<a style='color:rgb(255,127,0)'>" . $x[$i] . "</a>";
+            $j++;
+        } elseif($j == 3) {
+            $newString = $newString . "<a style='color:rgb(255,255,0)'>" . $x[$i] . "</a>";
+            $j++;
+        } elseif($j == 4) {
+            $newString = $newString . "<a style='color:rgb(0,255,0)'>" . $x[$i] . "</a>";
+            $j++;
+        } elseif($j == 5) {
+            $newString = $newString . "<a style='color:rgb(0,0,255)'>" . $x[$i] . "</a>";
+            $j++;
+        } elseif($j == 6) {
+            $newString = $newString . "<a style='color:rgb(255,0,255)'>" . $x[$i] . "</a>";
+            $j = 1;
+        }
+    }
+    return $newString;
 }
 ?>
